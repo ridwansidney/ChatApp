f1ae95634eae25d98321d1eb96bea8668e143823
ridwanmrahman <astonmartin1018@gmail.com>
2024-10-17 06:53:03 +1000

    Added Role Based Controls

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const roleAuthorization = require('./auth');
+app.post('/admin/promote', roleAuthorization('Super Admin'), (req, res) => {
+    // Logic for promoting a user
+    res.send('User promoted successfully');
+  });
+
 app.get('/', (req, res) => {
     res.send('ChatApp Backend is Running!');
 });
