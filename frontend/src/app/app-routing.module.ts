896067722aaaa9f0d8b8d4311ffc1cc42b62d6df
ridwanmrahman <astonmartin1018@gmail.com>
2024-10-17 05:19:41 +1000

    Created Frontend and Enabled Navigation Between pages

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes} from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { ChatComponent } from "./chat/chat.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+
+const routes: Routes = [
+  { path:'', component: LoginComponent},
+  { path:'dashboard', component: DashboardComponent},
+  { path:'chat/:groupId/:channelId', component: ChatComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
+
