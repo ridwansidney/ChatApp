6ae0f7cda5999ecd1047edf4e25d6d4ac1b8d9dc
ridwanmrahman <astonmartin1018@gmail.com>
2024-10-17 04:35:35 +1000

    Updated PORT to 3000

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     res.send('ChatApp Backend is Running!');
 });
 
-const PORT = 5000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
