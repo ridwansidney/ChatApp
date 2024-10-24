1bbc72b2ba8ffb6f9d02999f787e8ab16d36e12f
ridwanmrahman <astonmartin1018@gmail.com>
2024-10-25 08:05:06 +1000

    allowing access to specific routes

--- Backend/auth.js
+++ Backend/auth.js
@@ -1,10 +1,10 @@
-module.exports = function(role) {
-    return (req, res, next) => {
-      const user = req.body.user; // Assuming user data is passed in the request body
-      if (user && user.role === role) {
-        next();
-      } else {
-        res.status(403).json({ message: 'Forbidden: Insufficient privileges' });
-      }
-    };
+module.exports = function (role) {
+  return (req, res, next) => {
+    const user = req.body.user; // Assuming user data is passed in the request body
+    if (user && user.role === role) {
+      next();
+    } else {
+      res.status(403).json({ message: 'Forbidden: Insufficient privileges' });
+    }
   };
+};
